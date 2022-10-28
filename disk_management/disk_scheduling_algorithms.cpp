41e3a8b671d23a86be88b116bf512ebf1de6808e
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-10-28 13:33:13 +0800

    Disk Managment section is studied.

--- disk_management/disk_scheduling_algorithms.cpp
+++ disk_management/disk_scheduling_algorithms.cpp
@@ -27,254 +27,210 @@
 
 using namespace std;
 
-const int MAXQUEUE = 200; //定义队列最大数
+const int MAX_TRACK_SIZE = 200; //定义队列最大数
 
 //结构体定义
-typedef struct node
-{
-  int go;
-  int visited;
-} qu;
+typedef struct node {
+  int track_number;
+  int finished;
+} disk_track;
 
-qu queue[MAXQUEUE];
-int quantity;
+disk_track queue[MAX_TRACK_SIZE];
+int queue_size;
 
 int start; //定义开始时磁头所在位置
 
 //初始化函数
-void initial()
-{
+void init() {
   int i;
 
-  for (i = 0; i < MAXQUEUE; i++)
-  {
-    queue[i].go = -1;
-    queue[i].visited = 0;
+  for (i = 0; i < MAX_TRACK_SIZE; i++) {
+    queue[i].track_number = -1;
+    queue[i].finished = 0;
   }
 
   start = 53; //磁头的初始位置
 }
 
 //读入磁道号流
-void readData()
-{
+void readInput() {
   FILE *fp;
   char fname[] = "disk_tracks.txt";
   int temp, i;
 
   cout << "请输入磁道号流文件名:";
 
-  if ((fp = fopen(fname, "r")) == NULL)
-  {
+  if ((fp = fopen(fname, "r")) == NULL) {
     cout << "错误,文件打不开,请检查文件名:)" << endl;
-  }
-  else
-  {
-    while (!feof(fp))
-    {
+  } else {
+    while (!feof(fp)) {
       fscanf(fp, "%d ", &temp);
-      queue[quantity].go = temp;
-      quantity++;
+      queue[queue_size].track_number = temp;
+      queue_size++;
     }
-    cout << endl
-         << "---------------------------------------------" << endl;
+    cout << endl << "---------------------------------------------" << endl;
     cout << "所读入的磁道号流:";
-    for (i = 0; i < quantity; i++)
-    {
-      cout << queue[i].go << "  ";
+    for (i = 0; i < queue_size; i++) {
+      cout << queue[i].track_number << "  ";
     }
-    cout << endl
-         << "请求数为:" << quantity << endl;
+    cout << endl << "请求数为:" << queue_size << endl;
   }
 }
 
 // FIFO算法
-void FIFO()
-{
+void fifo() {
   int i;
   int total = 0;
   int current;
 
-  cout << endl
-       << "---------------------------------------------" << endl;
+  cout << endl << "---------------------------------------------" << endl;
   cout << "FIFO算法的访问磁道号顺序流:";
 
   current = start;
-  for (i = 0; i < quantity; i++)
-  {
-    cout << queue[i].go << "  ";
-    total += abs(queue[i].go - current);
-    current = queue[i].go;
+  for (i = 0; i < queue_size; i++) {
+    cout << queue[i].track_number << "  ";
+    total += abs(queue[i].track_number - current);
+    current = queue[i].track_number;
   }
-  cout << endl
-       << "磁头移过的柱面数:" << total;
+  cout << endl << "磁头移过的柱面数:" << total;
 }
 
 //最短寻道优先调度算法
-void shortest()
-{
+void shortest() {
   int i, j, p;
   int total = 0;
   int current;
 
-  cout << endl
-       << "---------------------------------------------" << endl;
+  cout << endl << "---------------------------------------------" << endl;
   cout << "最短寻道优先调度算法的访问磁道号顺序流:";
 
   current = start;
-  for (i = 0; i < quantity; i++)
-  {
+  for (i = 0; i < queue_size; i++) {
     p = 0;
-    while (queue[p].visited != 0)
-    {
+    while (queue[p].finished != 0) {
       p++;
     }
-    for (j = p; j < quantity; j++)
-    {
-      if ((queue[j].visited == 0) && (abs(current - queue[p].go) > abs(current - queue[j].go)))
-      {
+    for (j = p; j < queue_size; j++) {
+      if ((queue[j].finished == 0) && (abs(current - queue[p].track_number) >
+                                       abs(current - queue[j].track_number))) {
         p = j;
       }
     }
-    cout << queue[p].go << "  ";
-    total += abs(queue[p].go - current);
-    queue[p].visited = 1;
-    current = queue[p].go;
+    cout << queue[p].track_number << "  ";
+    total += abs(queue[p].track_number - current);
+    queue[p].finished = 1;
+    current = queue[p].track_number;
   }
-  cout << endl
-       << "磁头移过的柱面数:" << total;
+  cout << endl << "磁头移过的柱面数:" << total;
 }
 
 //电梯算法
-void elevator()
-{
+void elevator() {
   int i, j, p, flag;
   int total = 0;
   int current;
 
-  cout << endl
-       << "---------------------------------------------" << endl;
+  cout << endl << "---------------------------------------------" << endl;
   cout << "电梯调度算法" << endl;
 
   //磁头初始向里
   cout << "磁头初始向里的访问磁道号顺序流:";
 
   current = start;
-  for (i = 0; i < quantity; i++)
-  {
+  for (i = 0; i < queue_size; i++) {
     flag = 1000;
     p = -1;
-    for (j = 0; j < quantity; j++)
-    {
-      if ((queue[j].visited == 0) && (queue[j].go >= current))
-      {
-        if (abs(queue[j].go - current) < flag)
-        {
+    // Find the next track bigger than the current one to increasing direction
+    for (j = 0; j < queue_size; j++) {
+      if ((queue[j].finished == 0) && (queue[j].track_number >= current)) {
+        if (abs(queue[j].track_number - current) < flag) {
           p = j;
-          flag = abs(queue[j].go - current);
+          flag = abs(queue[j].track_number - current);
         }
       }
     }
-    if (p != -1)
-    {
-      cout << queue[p].go << "  ";
-      total += abs(queue[p].go - current);
-      current = queue[p].go;
-      queue[p].visited = 1;
-    }
-    else
-    {
-      for (j = 0; j < quantity; j++)
-      {
-        if ((queue[j].visited == 0) && (queue[j].go < current))
-        {
-          if (abs(queue[j].go - current) < flag)
-          {
+    if (p != -1) {
+      cout << queue[p].track_number << "  ";
+      total += abs(queue[p].track_number - current);
+      current = queue[p].track_number;
+      queue[p].finished = 1;
+    } else {
+      // Find the next track bigger than the current one to decreasing direction
+      for (j = 0; j < queue_size; j++) {
+        if ((queue[j].finished == 0) && (queue[j].track_number < current)) {
+          if (abs(queue[j].track_number - current) < flag) {
             p = j;
-            flag = abs(queue[j].go - current);
+            flag = abs(queue[j].track_number - current);
           }
         }
       }
-      cout << queue[p].go << "  ";
-      total += abs(queue[p].go - current);
-      current = queue[p].go;
-      queue[p].visited = 1;
+      cout << queue[p].track_number << "  ";
+      total += abs(queue[p].track_number - current);
+      current = queue[p].track_number;
+      queue[p].finished = 1;
     }
   }
-  cout << endl
-       << "磁头移过的柱面数:" << total << endl;
+  cout << endl << "磁头移过的柱面数:" << total << endl;
 
   //磁头初始向外
-  for (i = 0; i < quantity; i++)
-  {
-    queue[i].visited = 0;
+  for (i = 0; i < queue_size; i++) {
+    queue[i].finished = 0;
   }
   total = 0;
 
   cout << "磁头初始向外的访问磁道号顺序流:";
 
   current = start;
-  for (i = 0; i < quantity; i++)
-  {
+  for (i = 0; i < queue_size; i++) {
     flag = 1000;
     p = -1;
-    for (j = 0; j < quantity; j++)
-    {
-      if ((queue[j].visited == 0) && (queue[j].go <= current))
-      {
-        if (abs(queue[j].go - current) < flag)
-        {
+    for (j = 0; j < queue_size; j++) {
+      if ((queue[j].finished == 0) && (queue[j].track_number <= current)) {
+        if (abs(queue[j].track_number - current) < flag) {
           p = j;
-          flag = abs(queue[j].go - current);
+          flag = abs(queue[j].track_number - current);
         }
       }
     }
-    if (p != -1)
-    {
-      cout << queue[p].go << "  ";
-      total += abs(queue[p].go - current);
-      current = queue[p].go;
-      queue[p].visited = 1;
-    }
-    else
-    {
-      for (j = 0; j < quantity; j++)
-      {
-        if ((queue[j].visited == 0) && (queue[j].go > current))
-        {
-          if (abs(queue[j].go - current) < flag)
-          {
+    if (p != -1) {
+      cout << queue[p].track_number << "  ";
+      total += abs(queue[p].track_number - current);
+      current = queue[p].track_number;
+      queue[p].finished = 1;
+    } else {
+      for (j = 0; j < queue_size; j++) {
+        if ((queue[j].finished == 0) && (queue[j].track_number > current)) {
+          if (abs(queue[j].track_number - current) < flag) {
             p = j;
-            flag = abs(queue[j].go - current);
+            flag = abs(queue[j].track_number - current);
           }
         }
       }
-      cout << queue[p].go << "  ";
-      total += abs(queue[p].go - current);
-      current = queue[p].go;
-      queue[p].visited = 1;
+      cout << queue[p].track_number << "  ";
+      total += abs(queue[p].track_number - current);
+      current = queue[p].track_number;
+      queue[p].finished = 1;
     }
   }
-  cout << endl
-       << "磁头移过的柱面数:" << total;
+  cout << endl << "磁头移过的柱面数:" << total;
 }
 
-void main()
-{
+int main() {
   int i;
 
-  initial();
+  init();
 
-  readData();
+  readInput();
 
-  FIFO();
+  fifo();
 
   shortest();
 
-  for (i = 0; i < quantity; i++)
-  {
-    queue[i].visited = 0;
+  for (i = 0; i < queue_size; i++) {
+    queue[i].finished = 0;
   }
 
   elevator();
+
+  return 0;
 }
--- disk_management/disk_space_management_algorithms.cpp
+++ disk_management/disk_space_management_algorithms.cpp
@@ -1,24 +1,27 @@
 /*
-五 磁盘存储空间的管理
+磁盘存储空间的管理
 主要有：
-位示图 和内存的位示差不多
-空闲块表 和可变内存管理差不多
+位示图和内存的位示图管理相似
+空闲块表和可变内存管理相似
 空闲块链 主要是UNIX成组链接法的设计与实现
 
-UNIX系统文件管理组连接算法：
+本程序包括:UNIX的成组链接法的设计与实现
 
+UNIX系统文件管理组连接算法：
 把空闲块分成若干组，把指向一组中各空闲块的指针集中一起。
 这样既可方便查找，又可减少为修改指针而启动磁盘的次数。
 UNIX系统：采用空闲块成组连接的方法。
 UNIX系统把每100个空闲块作为一组，每一组的第一个空闲块中登记下一组空闲块的块号和空闲块数，
 余下不足100块的那部分空闲块的块号及块数登记在一个专用块中，登记最后一组块号的那个空闲块其中第2个单元填“0”，
 表示该块中指出的块号是最后一组的块号，空闲块链到此结束。
-系统初始化时先把专用块内容读到内存，当需分配空闲块时，就直接在内存中可找到哪些块强障械模糠峙湟豢楹蟀芽障锌槭。
+
+系统初始化时先把专用块内容读到内存，当需分配空闲块时，就直接在内存中可找到哪些块是空闲的，每分配一块后把空闲块数减一。
 但要把一组中的第一个空闲块分配出去之前应把登记在该块中的下一组的块号及块数保存到专用块中。
 当一组空闲块被分配完后，则再把专用块的内容读到内存，指出另一组可供分配的空闲块。当归还一块时，
-只要把归还块的块号登记到当前组中且空闲块数加1。如果当前组已满100块，则把内存中的内容写到归还的那块中，
+只要把归还块的块号登记到当前组中，且空闲块数加1。如果当前组已满100块，则把内存中的内容写到归还的那块中，
 该归还块作为新组的第一块。假设初始化时系统已把专用块读入内存L单元开始的区域中，分配和回收的算法如下：
-  分配一个空闲块
+
+（1）分配一个空闲块
 查L单元内容（空闲块数）：
 当空闲块数1 i  ＝L＋空闲块数；
 从i单元得到一空闲块号；
@@ -29,7 +32,8 @@ UNIX系统把每100个空闲块作为一组，每一组的第一个空闲块中�
 不等于零把该块内容复制到专用块；
 该块分配给申请者；
 把专用块内容读到主存L开始的区域。
-归还一块
+
+（2）归还一块
 查L单元的空闲块数；
 当空闲块数100 空闲块数加1；
 j  ＝L＋空闲块数；
@@ -40,263 +44,212 @@ j  ＝L＋空闲块数；
 采用成组连接后，分配回收磁盘块时均在内存中查找和修改，只是在一组空闲块分配完或空闲的磁盘块构成一组时才启动磁盘读写。
 比单块连接方式效率高。
 
-6月5日下午题是模拟UNIX的成组链接法的设计与实现
-
+例题：
 主要考的是利用文件输入一堆空闲块的号码，然后利用UNIX的成组链接法的管理方法，按照10块成一组，
 并且可以实现输入一个数字N,然后把N个空闲块占用，输出专用块的大小，空闲号。如果专用块的空间不够，
 把下一个成组的内容考入专用块。输出不要求写文件，但是要显示在屏幕上。
 
-   本程序包括:UNIX的成组链接法的设计与实现
+*/
 
-  VC++调试通过
-
-  (C)copyright by Neo
-
-  欢迎大家测试 请问题请Email:sony006@163.com
-  */
-
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
 using namespace std;
 
-const int MAXGROUP = 10; //定义组的大小
-const int MAXJOB = 100;  //定义一个作业最大能申请的块数
+const int MAX_BLOCK_SIZE_IN_GROUP = 10;       //定义组的大小
+const int MAX_TASK_SIZE = 100; //定义一个作业最大能申请的块数
 
 //结构体定义
-typedef struct node
-{
-  int quantity;
-  int cell[MAXGROUP];
+typedef struct node {
+  int size;
+  int blocks[MAX_BLOCK_SIZE_IN_GROUP];
   struct node *next;
 } group;
 
-typedef struct node1
-{
+typedef struct node1 {
   char name[20];
-  int quantity;
-  int cell[MAXJOB];
+  int block_size;
+  int blocks[MAX_TASK_SIZE];
   struct node1 *next;
-} job;
+} task;
 
-group *head;
-int total;
+group *groups_head;
+int total_free_block_size;
 
-job *jhead;
+task *tasks_head;
 
 //初始化组函数
-group *initial()
-{
+group *create_group() {
   int i;
   group *p;
 
   p = new group;
 
-  p->quantity = 0;
+  p->size = 0;
   p->next = NULL;
 
-  for (i = 0; i < MAXGROUP; i++)
-  {
-    p->cell[i] = -1;
+  for (i = 0; i < MAX_BLOCK_SIZE_IN_GROUP; i++) {
+    p->blocks[i] = -1;
   }
 
   return p;
 }
 
 //初始化作业函数
-job *initial_job()
-{
+task *create_task() {
   int i;
-  job *p;
+  task *p;
 
-  p = new job;
+  p = new task;
 
   strcpy(p->name, "");
-  p->quantity = 0;
+  p->block_size = 0;
   p->next = NULL;
 
-  for (i = 0; i < MAXGROUP; i++)
-  {
-    p->cell[i] = -1;
+  for (i = 0; i < MAX_BLOCK_SIZE_IN_GROUP; i++) {
+    p->blocks[i] = -1;
   }
 
   return p;
 }
 
 //读入空闲块流文件
-void readData()
-{
+void readInput() {
   FILE *fp;
   char fname[] = "free_disk_blocks.txt";
   int temp;
   group *p;
 
-  cout << "请输入初始空闲块数据文件名:";
-
-  if ((fp = fopen(fname, "r")) == NULL)
-  {
+  if ((fp = fopen(fname, "r")) == NULL) {
     cout << "错误,文件打不开,请检查文件名:)" << endl;
-  }
-  else
-  {
+  } else {
     cout << "==================================================" << endl;
     cout << "读入的初始空闲块号:";
-    while (!feof(fp))
-    {
+    while (!feof(fp)) {
       fscanf(fp, "%d ", &temp);
-      if (head->quantity < MAXGROUP)
-      {
-        head->cell[head->quantity] = temp;
-        head->quantity++;
+      if (groups_head->size < MAX_BLOCK_SIZE_IN_GROUP) {
+        groups_head->blocks[groups_head->size] = temp;
+        groups_head->size++;
+      } else {
+        p = create_group();
+        p->next = groups_head;
+        groups_head = p;
+        p->blocks[p->size] = temp;
+        p->size++;
       }
-      else
-      {
-        p = initial();
-        p->next = head;
-        head = p;
-        p->cell[p->quantity] = temp;
-        p->quantity++;
-      }
-      total++;
+      total_free_block_size++;
       //输出初始数据
       cout << temp << "  ";
     }
 
-    cout << endl
-         << "总空闲块数:" << total << endl;
+    cout << endl << "总空闲块数:" << total_free_block_size << endl;
   }
 }
 
 //查看专用块函数
-void view()
-{
+void view() {
   int i;
 
-  cout << endl
-       << "专用块数据如下:" << endl;
+  cout << endl << "专用块数据如下:" << endl;
   cout << "-------------------------------" << endl;
   cout << "所存储的空闲块号:";
-  for (i = 0; i < head->quantity; i++)
-  {
-    cout << head->cell[i] << "  ";
+  for (i = 0; i < groups_head->size; i++) {
+    cout << groups_head->blocks[i] << "  ";
   }
-  cout << endl
-       << "专用块空闲块数为:" << head->quantity;
-  cout << endl
-       << "总空闲块数:" << total << endl;
+  cout << endl << "专用块空闲块数为:" << groups_head->size;
+  cout << endl << "总空闲块数:" << total_free_block_size << endl;
 }
 
 //新申请函数
-void bid()
-{
-  char jobname[20];
-  int number;
+void allocate() {
+  char task_name[20];
+  int block_size;
   int i;
-  job *p;
+  task *p;
 
   cout << "----------------------------------" << endl;
   cout << "请输入新专业名:";
-  cin >> jobname;
+  cin >> task_name;
   cout << "所需内存块数:";
-  cin >> number;
+  cin >> block_size;
 
-  if (number > total)
-  {
+  if (block_size > total_free_block_size) {
     cout << "所需内存块数大于当前空闲块数,请稍候再试:)" << endl;
-  }
-  else
-  {
-    p = initial_job();
-    strcpy(p->name, jobname);
-    p->next = jhead->next;
-    jhead->next = p;
-    p->quantity = number;
+  } else {
+    p = create_task();
+    strcpy(p->name, task_name);
+    p->next = tasks_head->next;
+    tasks_head->next = p;
+    p->block_size = block_size;
     cout << "所申请到的空闲块号流:";
-    for (i = 0; i < number; i++)
-    {
-      if (head->quantity > 1)
-      {
-        cout << head->cell[head->quantity - 1] << "  ";
-        head->quantity--;
-        p->cell[i] = head->cell[head->quantity - 1];
-      }
-      else
-      {
-        cout << head->cell[0] << "  ";
-        p->cell[i] = head->cell[head->quantity - 1];
-        head->quantity--;
-        if (head->next != NULL)
-        {
-          head = head->next;
+    for (i = 0; i < block_size; i++) {
+      if (groups_head->size > 1) {
+        cout << groups_head->blocks[groups_head->size - 1]
+             << "  ";
+        groups_head->size--;
+        p->blocks[i] = groups_head->blocks[groups_head->size - 1];
+      } else {
+        cout << groups_head->blocks[0] << "  ";
+        p->blocks[i] = groups_head->blocks[groups_head->size - 1];
+        groups_head->size--;
+        if (groups_head->next != NULL) {
+          groups_head = groups_head->next;
         }
       }
-      total--;
+      total_free_block_size--;
     }
   }
   cout << endl;
 }
 
 //撤消作业
-void finish()
-{
-  char jobname[20];
+void release() {
+  char task_name[20];
   int i;
-  job *p, *q;
+  task *p, *q;
   group *r;
 
   cout << "请输入要撤消的作业名:";
-  cin >> jobname;
+  cin >> task_name;
 
-  q = jhead;
-  p = jhead->next;
-  while ((p != NULL) && (strcmp(p->name, jobname)))
-  {
+  q = tasks_head;
+  p = tasks_head->next;
+  while ((p != NULL) && (strcmp(p->name, task_name))) {
     q = q->next;
     p = p->next;
   }
 
-  if (p == NULL)
-  {
+  if (p == NULL) {
     cout << "Sorry,没有该作业" << endl;
-  }
-  else
-  {
-    for (i = 0; i < p->quantity; i++)
-    {
-      if (head->quantity < MAXGROUP)
-      {
-        head->cell[head->quantity] = p->cell[i];
-        head->quantity++;
-      }
-      else
-      {
-        r = initial();
-        r->next = head;
-        head = r;
-        r->cell[r->quantity] = p->cell[i];
-        r->quantity++;
+  } else {
+    for (i = 0; i < p->block_size; i++) {
+      if (groups_head->size < MAX_BLOCK_SIZE_IN_GROUP) {
+        groups_head->blocks[groups_head->size] = p->blocks[i];
+        groups_head->size++;
+      } else {
+        r = create_group();
+        r->next = groups_head;
+        groups_head = r;
+        r->blocks[r->size] = p->blocks[i];
+        r->size++;
       }
     }
-    total += p->quantity;
+    total_free_block_size += p->block_size;
 
     q->next = p->next;
     delete p;
   }
 }
 
-int main()
-{
+int main() {
   int f = 1;
   int chioce;
 
-  head = initial();
-  total = 0;
-  jhead = initial_job();
+  groups_head = create_group();
+  total_free_block_size = 0;
+  tasks_head = create_task();
 
-  readData();
+  readInput();
 
-  while (f == 1)
-  {
+  while (f == 1) {
     cout << "====================================================" << endl;
     cout << "           模拟UNIX的成组链接法    " << endl;
     cout << "====================================================" << endl;
@@ -304,13 +257,12 @@ int main()
     cout << "请选择:";
     cin >> chioce;
 
-    switch (chioce)
-    {
+    switch (chioce) {
     case 1:
-      bid();
+      allocate();
       break;
     case 2:
-      finish();
+      release();
       break;
     case 3:
       view();
