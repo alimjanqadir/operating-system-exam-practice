fd82194f288fefe1ca7b1180bc016d0713a989e1
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-10-29 09:38:05 +0800

    Dead Lock section studied but needs further review.

--- dead_lock/bankers_algorithm.cpp
+++ dead_lock/bankers_algorithm.cpp
@@ -1,15 +1,15 @@
 /*
 银行家算法
 
-银行家可以把一定数量的资金供多个用户周转使用，为保证资金的安全银行家规定：
+银行家可以把一定数量的资金供多个用户周转使用，为保证资金的安全，银行家规定：
 
 1.当一个用户对资金的最大需求量不超过银行家现有的资金就要接纳该用户;
 2.用户可以分期贷款，但贷款的总数不能超过最大需求量;
-3.当银行家现有的资金不能满足用户向需的贷款数时,对用户的贷款可推迟支付,但总能使用户在有限的时间里得到贷款;
+3.当银行家现有的资金不能满足用户向需的贷款数时，对用户的贷款可推迟支付,但总能使用户在有限的时间里得到贷款;
 4.当用户得到所需的全部资金后,一定能在有限的时间里归还所有的资金.
 
 假设共有3类资源A B C,分别为10,5,7个
-进程列表存在文件中,格式为
+进程列表存在文件中，格式为
 
 进程名 对A类资源需求数量  对B类资源需求数量  对c资源需求数量
 
@@ -17,81 +17,69 @@
 
 
 本程序包括:银行家算法
-
-
-  VC++调试通过
-
-  (C)copyright by Neo
-
-  欢迎大家测试 请问题请Email:sony006@163.com
 */
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-const int MAX_P = 20;
-const int MAXA = 10; //定义A类资源的数量
-const int MAXB = 5;
-const int MAXC = 7;
+const int MAX_PROCESS = 20;
+const int RESOURCE_A_SIZE = 10; //定义A类资源的数量
+const int RESOURCE_B_SIZE = 5;
+const int RESOURCE_C_SIZE = 7;
 
-typedef struct node
-{
-  int a;
-  int b;
-  int c;
-  int remain_a;
-  int remain_b;
-  int remain_c;
+typedef struct node {
+  int allocated_a;
+  int allocated_b;
+  int allocated_c;
+  int remained_a;
+  int remained_b;
+  int remained_c;
 } bank;
 
-typedef struct node1
-{
+typedef struct node1 {
   char name[20];
-  int a;
-  int b;
-  int c;
-  int need_a;
-  int need_b;
-  int need_c;
+  int allocated_a;
+  int allocated_b;
+  int allocated_c;
+  int remained_a;
+  int remained_b;
+  int remained_c;
 } process;
 
 bank banker;
-process processes[MAX_P];
-int quantity;
+process processes[MAX_PROCESS];
+int process_size;
 
 //初始化函数
-void initial()
-{
+void init() {
   int i;
 
-  banker.a = MAXA;
-  banker.b = MAXB;
-  banker.c = MAXC;
-  banker.remain_a = MAXA;
-  banker.remain_b = MAXB;
-  banker.remain_c = MAXC;
+  banker.allocated_a = RESOURCE_A_SIZE;
+  banker.allocated_b = RESOURCE_B_SIZE;
+  banker.allocated_c = RESOURCE_C_SIZE;
+  banker.remained_a = RESOURCE_A_SIZE;
+  banker.remained_b = RESOURCE_B_SIZE;
+  banker.remained_c = RESOURCE_C_SIZE;
 
-  for (i = 0; i < MAX_P; i++)
-  {
+  for (i = 0; i < MAX_PROCESS; i++) {
     strcpy(processes[i].name, "");
-    processes[i].a = 0;
-    processes[i].b = 0;
-    processes[i].c = 0;
-    processes[i].need_a = 0;
-    processes[i].need_b = 0;
-    processes[i].need_c = 0;
+    processes[i].allocated_a = 0;
+    processes[i].allocated_b = 0;
+    processes[i].allocated_c = 0;
+    processes[i].remained_a = 0;
+    processes[i].remained_b = 0;
+    processes[i].remained_c = 0;
   }
 }
 
 //新加作业
-void add()
-{
+void add_task() {
   char name[20];
   int flag = 0;
   int t;
-  int need_a, need_b, need_c;
+  int resource_a, resource_b, resource_c;
   int i;
 
   cout << endl;
@@ -100,87 +88,71 @@ void add()
   cout << "请输入新加作业名:";
   cin >> name;
 
-  for (i = 0; i < quantity; i++)
-  {
-    if (!strcmp(processes[i].name, name))
-    {
+  for (i = 0; i < process_size; i++) {
+    if (!strcmp(processes[i].name, name)) {
       flag = 1;
       break;
     }
   }
 
-  if (flag)
-  {
+  if (flag) {
     cout << "错误,作业已存在" << endl;
-  }
-  else
-  {
+  } else {
     cout << "本作业所需A类资源:";
-    cin >> need_a;
+    cin >> resource_a;
     cout << "本作业所需B类资源:";
-    cin >> need_b;
+    cin >> resource_b;
     cout << "本作业所需C类资源:";
-    cin >> need_c;
+    cin >> resource_c;
 
     t = 1;
-    cout << need_a << banker.remain_a;
-    if (need_a > banker.remain_a)
-    {
+    cout << resource_a << banker.remained_a;
+    if (resource_a > banker.remained_a) {
       cout << "错误,所需A类资源大于银行家所剩A类资源" << endl;
       t = 0;
     }
-    if (need_b > banker.remain_b)
-    {
+    if (resource_b > banker.remained_b) {
       cout << "错误,所需B类资源大于银行家所剩B类资源" << endl;
       t = 0;
     }
-    if (need_c > banker.remain_c)
-    {
+    if (resource_c > banker.remained_c) {
       cout << "错误,所需C类资源大于银行家所剩C类资源" << endl;
       t = 0;
     }
-    if (t)
-    {
-      strcpy(processes[quantity].name, name);
-      processes[quantity].need_a = need_a;
-      processes[quantity].need_b = need_b;
-      processes[quantity].need_c = need_c;
+    if (t) {
+      strcpy(processes[process_size].name, name);
+      processes[process_size].remained_a = resource_a;
+      processes[process_size].remained_b = resource_b;
+      processes[process_size].remained_c = resource_c;
 
-      quantity++;
+      process_size++;
       cout << "新加作业成功" << endl;
-    }
-    else
-    {
+    } else {
       cout << "新加作业失败" << endl;
     }
   }
 }
 
 //为作业申请资源
-void bid()
-{
+void alloc() {
   char name[20];
   int i, p;
   int a, b, c;
   int flag;
 
-  cout << endl
-       << "为作业申请资源" << endl;
+  cout << endl << "为作业申请资源" << endl;
   cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
   cout << "要申请资源的作业名:";
   cin >> name;
 
   p = -1;
-  for (i = 0; i < quantity; i++)
-  {
-    if (!strcmp(processes[i].name, name))
-    {
+  for (i = 0; i < process_size; i++) {
+    if (!strcmp(processes[i].name, name)) {
       p = i;
       break;
     }
   }
-  if (p != -1)
-  {
+  if (p != -1) {
     cout << "该作业要申请A类资源数量:";
     cin >> a;
     cout << "该作业要申请B类资源数量:";
@@ -189,151 +161,129 @@ void bid()
     cin >> c;
 
     flag = 1;
-    if ((a > banker.remain_a) || (a > processes[p].need_a - processes[p].a))
-    {
+    if ((a > banker.remained_a) ||
+        (a > processes[p].remained_a - processes[p].allocated_a)) {
       cout << "错误,所申请A类资源大于银行家所剩A类资源或该进程还需数量" << endl;
       flag = 0;
     }
-    if ((b > banker.remain_b) || (b > processes[p].need_b - processes[p].b))
-    {
+    if ((b > banker.remained_b) ||
+        (b > processes[p].remained_b - processes[p].allocated_b)) {
       cout << "错误,所申请B类资源大于银行家所剩B类资源或该进程还需数量" << endl;
       flag = 0;
     }
-    if ((c > banker.remain_c) || (c > processes[p].need_c - processes[p].c))
-    {
+    if ((c > banker.remained_c) ||
+        (c > processes[p].remained_c - processes[p].allocated_c)) {
       cout << "错误,所申请C类资源大于银行家所剩C类资源或该进程还需数量" << endl;
       flag = 0;
     }
-    if (flag)
-    {
-      banker.remain_a -= a;
-      banker.remain_b -= b;
-      banker.remain_c -= c;
+    if (flag) {
+      banker.remained_a -= a;
+      banker.remained_b -= b;
+      banker.remained_c -= c;
 
-      processes[p].a += a;
-      processes[p].b += b;
-      processes[p].c += c;
+      processes[p].allocated_a += a;
+      processes[p].allocated_b += b;
+      processes[p].allocated_c += c;
 
       cout << "为作业申请资源成功" << endl;
-    }
-    else
-    {
+    } else {
       cout << "为作业申请资源失败" << endl;
     }
-  }
-  else
-  {
+  } else {
     cout << "该作业不存在" << endl;
   }
 }
 
 //撤消作业
-void finished()
-{
+void release() {
   char name[20];
   int i, p;
 
-  cout << endl
-       << "撤消作业" << endl;
+  cout << endl << "撤消作业" << endl;
   cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
   cout << "要撤消作业名:";
   cin >> name;
 
   p = -1;
-  for (i = 0; i < quantity; i++)
-  {
-    if (!strcmp(processes[i].name, name))
-    {
+  for (i = 0; i < process_size; i++) {
+    if (!strcmp(processes[i].name, name)) {
       p = i;
       break;
     }
   }
-  if (p != -1)
-  {
-    banker.remain_a += processes[p].a;
-    banker.remain_b += processes[p].b;
-    banker.remain_c += processes[p].c;
-    for (i = p; i < quantity - 1; i++)
-    {
+  if (p != -1) {
+    banker.remained_a += processes[p].allocated_a;
+    banker.remained_b += processes[p].allocated_b;
+    banker.remained_c += processes[p].allocated_c;
+    for (i = p; i < process_size - 1; i++) {
       processes[i] = processes[i + 1];
     }
-    strcpy(processes[quantity - 1].name, "");
-    processes[quantity - 1].a = 0;
-    processes[quantity - 1].b = 0;
-    processes[quantity - 1].c = 0;
-    processes[quantity - 1].need_a = 0;
-    processes[quantity - 1].need_b = 0;
-    processes[quantity - 1].need_c = 0;
+    strcpy(processes[process_size - 1].name, "");
+    processes[process_size - 1].allocated_a = 0;
+    processes[process_size - 1].allocated_b = 0;
+    processes[process_size - 1].allocated_c = 0;
+    processes[process_size - 1].remained_a = 0;
+    processes[process_size - 1].remained_b = 0;
+    processes[process_size - 1].remained_c = 0;
 
-    quantity--;
+    process_size--;
 
     cout << "撤消作业成功" << endl;
-  }
-  else
-  {
+  } else {
     cout << "撤消作业失败" << endl;
   }
 }
 
 //查看资源情况
-void view()
-{
+void view() {
   int i;
 
-  cout << endl
-       << "查看资源情况" << endl;
+  cout << endl << "查看资源情况" << endl;
   cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
   cout << "银行家所剩资源(剩余资源/总共资源)" << endl;
-  cout << "A类:" << banker.remain_a << "/" << banker.a;
-  cout << "   B类:" << banker.remain_b << "/" << banker.b;
-  cout << "   C类:" << banker.remain_c << "/" << banker.c;
+  cout << "A类:" << banker.remained_a << "/" << banker.allocated_a;
+  cout << "   B类:" << banker.remained_b << "/" << banker.allocated_b;
+  cout << "   C类:" << banker.remained_c << "/" << banker.allocated_c;
 
-  cout << endl
-       << endl
-       << "作业占用情况(已占用资源/所需资源)" << endl
-       << endl;
-  if (quantity > 0)
-  {
-    for (i = 0; i < quantity; i++)
-    {
+  cout << endl << endl << "作业占用情况(已占用资源/所需资源)" << endl << endl;
+  if (process_size > 0) {
+    for (i = 0; i < process_size; i++) {
       cout << "作业名:" << processes[i].name << endl;
-      cout << "A类:" << processes[i].a << "/" << processes[i].need_a;
-      cout << "   B类:" << processes[i].b << "/" << processes[i].need_b;
-      cout << "   C类:" << processes[i].c << "/" << processes[i].need_c;
+      cout << "A类:" << processes[i].allocated_a << "/"
+           << processes[i].remained_a;
+      cout << "   B类:" << processes[i].allocated_b << "/"
+           << processes[i].remained_b;
+      cout << "   C类:" << processes[i].allocated_c << "/"
+           << processes[i].remained_c;
       cout << endl;
     }
-  }
-  else
-  {
+  } else {
     cout << "当前没有作业" << endl;
   }
 }
 
-int main()
-{
+int main() {
   int chioce;
   int flag = 1;
 
-  initial();
+  init();
 
-  while (flag)
-  {
+  while (flag) {
     cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << endl;
     cout << "1.新加作业  2.为作业申请资源  3.撤消作业" << endl;
     cout << "4.查看资源情况   0.退出系统" << endl;
     cout << "请选择:";
     cin >> chioce;
 
-    switch (chioce)
-    {
+    switch (chioce) {
     case 1:
-      add();
+      add_task();
       break;
     case 2:
-      bid();
+      alloc();
       break;
     case 3:
-      finished();
+      release();
       break;
     case 4:
       view();
@@ -342,8 +292,7 @@ int main()
       flag = 0;
       break;
     default:
-      cout << "选择错误" << endl
-           << endl;
+      cout << "选择错误" << endl << endl;
     }
   }
   return 0;
